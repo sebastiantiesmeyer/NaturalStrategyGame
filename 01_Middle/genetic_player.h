8191697dcaac3debc28d3230155efc96e313b3ca
sebastiantiesmeyer <sebastian.tiesmeyer@gmail.com>
2017-05-31 14:33:30 +0200

    genetic_player

--- 01_Middle/genetic_player.h
+++ 01_Middle/genetic_player.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "abstract_player.h"
 #include "local.h"
+#include <random>
 
 class GeneticPlayer : public AbstractPlayer
 {
@@ -8,6 +9,18 @@ public:
 	GeneticPlayer(const AbstPlayerConstrParams &pars) : AbstractPlayer::AbstractPlayer(pars) {}
 protected:
 
+	std::default_random_engine rnd_engine;
+
+
+	int output[5] ;
+	int input[18];
+
+	float weights[sizeof(output)][sizeof(input)];
+
+	void init_weights(float scope);
+	void forward_pass();
+	void mutate(float scope);
+	void cross_over();
 
 
 	virtual void do_StartTurn();
