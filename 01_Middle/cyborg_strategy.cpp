9ab57203cda0b5d9caf979f252fffb9b0ce73c99
Csabix <csabix.balint@gmail.com>
2017-06-14 18:43:25 +0200

    for merge

--- 01_Middle/cyborg_strategy.cpp
+++ 01_Middle/cyborg_strategy.cpp
@@ -99,21 +99,21 @@ void CyborgStrategy::Render()
 
 	sprintf_s(buff, "%d/%d", unit_progress.progress[ROCK], (int)current_training_time);
 	ImGui::RadioButton("Rock", (int*)&queue.train, (int)ROCK); ImGui::SameLine(100);
-	if(queue.train == ROCK) ImGui::PushStyleColor(ImGuiCol_PlotHistogram, { 1,0.5,1,1 });
+	if(whattotrain == ROCK) ImGui::PushStyleColor(ImGuiCol_PlotHistogram, { 1,0.5,1,1 });
 	ImGui::ProgressBar(unit_progress.progress[ROCK] / current_training_time, { -1,0 }, buff);
-	if(queue.train == ROCK) ImGui::PopStyleColor();
+	if(whattotrain == ROCK) ImGui::PopStyleColor();
 
 	sprintf_s(buff, "%d/%d", unit_progress.progress[SCISSOR], (int)current_training_time);
 	ImGui::RadioButton("Scissor", (int*)&queue.train, (int)SCISSOR); ImGui::SameLine(100);
-	if(queue.train == SCISSOR) ImGui::PushStyleColor(ImGuiCol_PlotHistogram, { 1,0.5,1,1 });
+	if(whattotrain == SCISSOR) ImGui::PushStyleColor(ImGuiCol_PlotHistogram, { 1,0.5,1,1 });
 	ImGui::ProgressBar(unit_progress.progress[SCISSOR] / current_training_time, { -1,0 }, buff);
-	if(queue.train == SCISSOR) ImGui::PopStyleColor();
+	if(whattotrain == SCISSOR) ImGui::PopStyleColor();
 
 	sprintf_s(buff, "%d/%d", unit_progress.progress[PAPER], (int)current_training_time);
 	ImGui::RadioButton("Paper", (int*)&queue.train, (int)PAPER); ImGui::SameLine(100);
-	if(queue.train == PAPER) ImGui::PushStyleColor(ImGuiCol_PlotHistogram, { 1,0.5,1,1 });
+	if(whattotrain == PAPER) ImGui::PushStyleColor(ImGuiCol_PlotHistogram, { 1,0.5,1,1 });
 	ImGui::ProgressBar(unit_progress.progress[PAPER] / current_training_time, { -1,0 }, buff);
-	if(queue.train == PAPER) ImGui::PopStyleColor();
+	if(whattotrain == PAPER) ImGui::PopStyleColor();
 
 	ImGui::Separator();
 
